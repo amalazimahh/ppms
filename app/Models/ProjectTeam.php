82813cc7a4e3ae67aec763e7c4f3f6a6f2ea7a3a
amalazimahh <amalazimahh@gmail.com>
2025-02-28 12:49:44 +0800

    modify ProjectTeam model

--- app/Models/ProjectTeam.php
+++ app/Models/ProjectTeam.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProjectTeam extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'project_team';
+
+    protected $fillable =
+    [
+        'architect_id',
+        'mechanical_electrical_id',
+        'civil_structural_id',
+        'quantity_surveyor_id',
+        'others_id'
+    ];
+
+    // relationships
+    public function architect(){
+        return $this->belongsTo(Architect::class, 'architect_id');
+    }
+
+    public function mechanicalElectrical(){
+        return $this->belongsTo(MechanicalElectrical::class, 'mechanical_electrical_id');
+    }
+
+    public function civilStructural(){
+        return $this->belongsTo(CivilStructural::class, 'civil_structural_id');
+    }
+
+    public function quantitySurveyor(){
+        return $this->belongsTo(QuantitySurveyor::class, 'quantity_surveyor_id');
+    }
+
+    public function others(){
+        return $this->belongsTo(Others::class, 'others_id');
+    }
 }
