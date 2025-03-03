71c9134c1344e3807995cdfc34f02b87f15ba095
amalazimahh <amalazimahh@gmail.com>
2025-03-03 14:19:17 +0800

    modify navbar and footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,29 +3,29 @@
         <ul class="nav">
             <li class="nav-item">
                 <a href="https://creative-tim.com" target="blank" class="nav-link">
-                    {{ __('Creative Tim') }}
+                    {{ __('About PPMS') }}
                 </a>
             </li>
             <li class="nav-item">
                 <a href="https://updivision.com" target="blank" class="nav-link">
-                    {{ __('Updivision') }}
+                    {{ __('FAQ') }}
                 </a>
             </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
-                    {{ __('About Us') }}
+                    {{ __('Contact Us') }}
                 </a>
             </li>
             <li class="nav-item">
                 <a href="#" class="nav-link">
-                    {{ __('Blog') }}
+                    {{ __('Location') }}
                 </a>
             </li>
         </ul>
-        <div class="copyright">
+        <!-- <div class="copyright">
             &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ __('by') }}
             <a href="https://creative-tim.com" target="_blank">{{ __('Creative Tim') }}</a> &amp;
             <a href="https://updivision.com" target="_blank">{{ __('Updivision') }}</a> {{ __('for a better web') }}.
-        </div>
+        </div> -->
     </div>
 </footer>
