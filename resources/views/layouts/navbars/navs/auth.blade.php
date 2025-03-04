6f66a5cd8925da8f91193b8b9eee72e7944f535b
amalazimahh <amalazimahh@gmail.com>
2025-03-04 13:09:49 +0800

    comment settings and profile on nav bar

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -60,13 +60,13 @@
                         <p class="d-lg-none">{{ __('Log out') }}</p>
                     </a>
                     <ul class="dropdown-menu dropdown-navbar">
-                        <li class="nav-link">
+                        <!-- <li class="nav-link">
                             <a href="{{ route('profile.edit') }}" class="nav-item dropdown-item">{{ __('Profile') }}</a>
                         </li>
                         <li class="nav-link">
                             <a href="#" class="nav-item dropdown-item">{{ __('Settings') }}</a>
                         </li>
-                        <li class="dropdown-divider"></li>
+                        <li class="dropdown-divider"></li> -->
                         <li class="nav-link">
                             <a href="{{ route('logout') }}" class="nav-item dropdown-item" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
                         </li>
