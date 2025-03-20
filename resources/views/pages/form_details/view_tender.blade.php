94959e237bb92dbb4d4179df44c4095ea13bc63b
amalazimahh <amalazimahh@gmail.com>
2025-03-20 09:35:47 +0800

    fix UI of vertical tabs and its contents

---
+++ resources/views/pages/form_details/view_tender.blade.php
@@ -0,0 +1,15 @@
+<div class="mt-3">
+    <h3>Tender Details</h3>
+    <p><strong>Tender No.:</strong> </p>
+    <p><strong>Confirmation Fund:</strong> </p>
+    <p><strong>Cost Estimate Amount:</strong> </p>
+    <p><strong>Cost Estimate Date:</strong> </p>
+    <p><strong>Tender Opened First:</strong> </p>
+    <p><strong>Tender Opened Second:</strong> </p>
+    <p><strong>Tender Closed:</strong> </p>
+    <p><strong>Tender Extended:</strong> </p>
+    <p><strong>Tender Validity:</strong> </p>
+    <p><strong>Tender Validity Extended:</strong> </p>
+    <p><strong>Tender Cancelled:</strong> </p>
+
+</div>
---
+++ resources/views/pages/form_details/view_tender_recomm.blade.php
@@ -0,0 +1,10 @@
+<div class="mt-3">
+    <h3>Tender Recommendation Details</h3>
+    <p><strong>Recommendation to Consultant:</strong> </p>
+    <p><strong>Recommendation from Consultant:</strong> </p>
+    <p><strong>Recommendation from BPP:</strong> </p>
+    <p><strong>Recommendation to DG:</strong> </p>
+    <p><strong>Recommendation to LTK:</strong> </p>
+    <p><strong>LTK Approval:</strong> </p>
+    <p><strong>Discount Letter:</strong> </p>
+</div>
