c61edcd18c002672177ba7850d2c77d65480287f
amalazimahh <amalazimahh@gmail.com>
2025-03-26 11:26:10 +0800

    remove project_team_id

--- database/migrations/2025_02_26_020245_create_project_table.php
+++ database/migrations/2025_02_26_020245_create_project_table.php
@@ -16,24 +16,17 @@ return new class extends Migration
             $table->string('fy')->nullable();
             $table->decimal('sv', 15, 2)->nullable();
             $table->decimal('av', 15, 2)->nullable();
+            $table->foreignId('parent_project_id')->nullable()->constrained('project')->onDelete('cascade');
             $table->string('voteNum')->nullable();
             $table->string('title')->nullable();
+            $table->foreignId('client_ministry_id')->nullable()->constrained('client_ministry')->onDelete('cascade');
             $table->string('siteGazette')->nullable();
             $table->text('scope')->nullable();
             $table->text('location')->nullable();
             $table->string('img')->nullable();
             $table->timestamps();
 
-            // foreign keys
-            $table->foreignId('statuses_id')->nullable()->constrained('statuses')->onDelete('cascade');
-            $table->foreignId('parent_project_id')->nullable()->constrained('project')->onDelete('cascade');
-            $table->foreignId('client_ministry_id')->nullable()->constrained('client_ministry')->onDelete('cascade');
-            $table->foreignId('project_team_id')->nullable()->constrained('project_team')->onDelete('cascade');
-
-            // users
-            $table->bigInteger('oic')->unsigned()->nullable();
             $table->bigInteger('created_by')->unsigned()->nullable();
-            $table->foreign('oic')->references('id')->on('users')->onDelete('set null');
             $table->foreign('created_by')->references('id')->on('users')->onDelete('set null');
 
         });
