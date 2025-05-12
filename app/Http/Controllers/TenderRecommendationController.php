71ce288c1e3612d9805e18382e0f5d0dda36fab4
amalazimahh <amalazimahh@gmail.com>
2025-05-12 15:54:08 +0800

    fix spelling error for Project model

--- app/Http/Controllers/TenderRecommendationController.php
+++ app/Http/Controllers/TenderRecommendationController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Projects;
+use App\Models\Project;
 use App\Models\Tender;
 use App\Models\TenderRecommendation;
 use App\Models\Milestone;
