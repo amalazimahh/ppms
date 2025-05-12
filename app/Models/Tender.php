72cd2db9ffc325dc1fb56eb83521f06263b45cf4
amalazimahh <amalazimahh@gmail.com>
2025-05-12 15:31:12 +0800

    add relationship between project and tender to access milestones

--- app/Models/Tender.php
+++ app/Models/Tender.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Project;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -24,4 +25,9 @@ class Tender extends Model
         'validity_ext',
         'cancelled'
     ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
