c9c2862166fa104f3e2172c373177884a04f9685
amalazimahh <amalazimahh@gmail.com>
2025-05-19 22:42:53 +0800

    added assignRole method to assign roles to users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -17,5 +18,16 @@ class UserController extends Controller
     public function index(User $model)
     {
         return view('users.index', ['users' => $model->paginate(15)]);
+    }
+
+    public function assignRole(Request $request, User $user)
+    {
+        $request->validate([
+            'roles_id' => 'required|exists:roles,id',
+        ]);
+        $user->roles_id = $request->input('roles_id');
+        $user->save();
+
+        return redirect()->back()->with('status', 'Role assigned successfully!');
     }
 }
