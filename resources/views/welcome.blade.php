358106f942bd285f58eb6d0503478bf99c694645
amalazimahh <amalazimahh@gmail.com>
2025-05-22 15:32:10 +0800

    added a modal to alert user that their account still have unassigned user role

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,26 @@
+@if(session('role_pending'))
+<!-- Modal -->
+<div class="modal fade show" id="rolePendingModal" tabindex="-1" role="dialog" aria-labelledby="rolePendingModalLabel" aria-modal="true" style="display:block;">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="rolePendingModalLabel">Role Pending</h5>
+      </div>
+      <div class="modal-body">
+        Your account does not have a role assigned yet.<br>
+        Please wait for an administrator to assign your role.<br>
+        You will remain on the landing page as a guest.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" onclick="document.getElementById('rolePendingModal').style.display='none';">OK</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+document.body.classList.add('modal-open');
+</script>
+@endif
 @extends('layouts.app')
 
 @section('content')
