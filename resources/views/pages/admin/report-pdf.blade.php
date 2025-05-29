28d32aec4cf592d876cfd9d48a3fc211c6f2f8e2
amalazimahh <amalazimahh@gmail.com>
2025-05-29 11:28:15 +0800

    change report pdf size from A4 portrait to A3 landsccape

--- resources/views/pages/admin/report-pdf.blade.php
+++ resources/views/pages/admin/report-pdf.blade.php
@@ -10,26 +10,31 @@
             max-width: 800px;
             margin: auto;
             padding: 30px;
-            border: 1px solid #eee;
         }
         .title {
-            font-size: 36px;
-            color: #4caf50;
+            font-size: 26px;
+            color:rgb(165, 22, 170);
         }
     </style>
 </head>
 <body>
     <div class="invoice-box">
-        <div class="title">Project Summary Details</div>
-        <hr>
-        <p><strong>Financial Year:</strong> {{ $project->fy }}</p>
-        <p><strong>Vote No.:</strong> {{ $project->voteNum }}</p>
-        @if($project->parent_project_id)
-            <p><strong>Parent Project:</strong> {{ $project->parentProject->title }}</p>
-        @endif
-        <p><strong>Title:</strong> {{ $project->title }}</p>
-        <p><strong>Scheme Value: $</strong> {{ $project->sv }}</p>
-        <p><strong>Allocation Value: $</strong> {{ $project->av }}</p>
+        <div class="title">PROJECT DETAILS/SUMMARY REPORT</div>
+        <table class="table" width="100%" style="border-collapse: collapse; margin-top: 20px;">
+            <tr>
+                <td style="border: 1px solid #000; padding: 4px;"><strong>RKN:</strong> {{ $project->rkn_id->rknNum ?? '' }}</td>
+                <td style="border: 1px solid #000; padding: 4px;"><strong>Financial Year:</strong> {{ $project->fy }}</td>
+                <td style="border: 1px solid #000; padding: 4px;"><strong>Vote No.:</strong> {{ $project->voteNum }}</td>
+            </tr>
+            <tr>
+                <td style="border: 1px solid #000; padding: 4px;" colspan="3">
+                    @if($project->parent_project_id)
+                        <strong>Parent Project:</strong> {{ $project->parentProject->title }}<br>
+                    @endif
+                    <strong>Title:</strong> {{ $project->title }}
+                </td>
+            </tr>
+        </table>
     </div>
 </body>
 </html>
